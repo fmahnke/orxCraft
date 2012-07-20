e6e588c1829dbb15c8e3366b0859d6f6f7e2b811
Konrad Klimaszewski <graag@o2.pl>
2012-07-20 21:29:32 +0200

    Remove local files from CodeLite project.

--- build/codelite/OrxCraft/OrxCraft_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ OrxCraft - Linux Release ]----------
-	@"$(MAKE)" -f "OrxCraft.mk" && "$(MAKE)" -f "OrxCraft.mk" PostBuild
-clean:
-	@echo ----------Cleaning project:[ OrxCraft - Linux Release ]----------
-	@"$(MAKE)" -f "OrxCraft.mk" clean
